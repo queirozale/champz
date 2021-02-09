29e125e0a81607c0b1253b044ca17b730c564472
Alessandro Queiroz <queirozalessandro1@gmail.com>
2021-02-09 01:18:06 -0300

    Project structure

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
+import Navbar from "../components/navbar/navbar";
+import Carousel from "../components/carousel/carousel";
+import InfoSection from "../components/info_section/info_section";
+import Simulator from "../components/simulator/simulator";
+import Forms from "../components/forms/forms";
+import AboutUs from "../components/about_us/about_us";
+import Footer from "../components/footer/footer";
+
+
 function Home() {
-    return <div>Home</div>
+    return (
+        <div>
+            <Navbar />
+            <Carousel/>
+            <InfoSection/>
+            {/* <Simulator/> */}
+            <Forms/>
+            {/* <AboutUs/> */}
+            <Footer />
+        </div>
+    )
 }
 
 export default Home;
