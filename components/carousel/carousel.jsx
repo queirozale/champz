77c054a4b1db0cc405ca6e506fa69ea86f789116
Aníbal Henrique <annibalhsouza@gmail.com>
2021-02-13 03:29:38 -0300

    #adicionando Carrosel para transições das imagens em slide

--- components/carousel/carousel.jsx
+++ components/carousel/carousel.jsx
@@ -1,41 +1,88 @@
-import React from 'react';
+import React from 'react'
 import Carousel from 'react-material-ui-carousel'
-import {Paper, Button} from '@material-ui/core'
+import Image from 'next/image'
+import {
+  Button,
+  Card,
+  Typography,
+  Hidden,
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
 
-function Example(props)
-{
-    var items = [
+const useStyles = makeStyles((theme) =>({
+  root: {
+    height: 827,
+    align: 'center',
+    backgroundColor: 'black',
+    
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  imgCard: {
+    width: '1900',
+    height: '840'
+  },
+  pos: {
+    marginBottom: 12,
+  },
+}));
+
+function SliderFirstSectionHome(props){
+  var items = [
+    {
+      name: "Random Name #1",
+      description: "Img 1",
+      img: "/Slide1.jpeg",
+    },
+    {
+      name: "Random Name #2",
+      description: "Img 2!",
+      img: "/Slide2.jpeg",
+    },
+    {
+      name: "Random Name #3",
+      description: "Img 3!",
+      img: "/Slide3.jpeg",
+    },
+    {
+      name: "Random Name #4",
+      description: "Img 4!",
+      img: "/Slide4.jpeg",
+    },
+  ]
+
+  return (
+    <div>
+      <Carousel>
         {
-            name: "Random Name #1",
-            description: "Probably the most random thing you have ever seen!"
-        },
-        {
-            name: "Random Name #2",
-            description: "Hello World!"
+          items.map( (item, i) => <Item key={i} item={item} /> )
         }
-    ]
-
-    return (
-        <Carousel>
-            {
-                items.map( (item, i) => <Item key={i} item={item} /> )
-            }
-        </Carousel>
-    )
+      </Carousel>
+    </div>
+  )
 }
 
-function Item(props)
-{
-    return (
-        <Paper>
-            <h2>{props.item.name}</h2>
-            <p>{props.item.description}</p>
 
-            <Button className="CheckButton">
-                Check it out!
-            </Button>
-        </Paper>
-    )
+function Item(props){
+  const classes = useStyles();
+
+  return (
+    <Card>
+      <Image
+        alt="Mountains"
+        src={props.item.img}
+        layout="responsive"
+        width={1889}
+        height={848}
+      />
+    </Card>
+  )
 }
 
-export default Example;
+export default SliderFirstSectionHome;
