7b9b704f3affddf0a004bdfea870c47f5103f2ad
Aníbal Henrique <annibalhsouza@gmail.com>
2021-02-20 23:37:52 -0300

    adicionando navlinks

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -6,10 +6,12 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import SportsMmaIcon from '@material-ui/icons/SportsMma';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    display: 'flex',
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -17,26 +19,46 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  appBar: {
+    backgroundColor: 'black',
+  },
+  navButton: {
+    color: 'black'
+  }
 }));
 
-const Navbar = () => {
+export default function Navbar(props) {
   const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-          News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+  const navLink = [{title: 'Atletas'}, {title:'Investidor'}]
+  const navButton = 'entrar'
 
+  return (
+    <AppBar position="static" className={classes.appBar}>
+      <Toolbar>
+      <div className={classes.root}>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+        <MenuIcon />
+        </IconButton>
+        {
+          navLink.map((navLink, i) => <NavLink key={i} navLink={navLink} /> )
+        }
+      </div>
+      <Button variant="contained" color="inherit" className={classes.navButton}>{navButton}</Button>
+      </Toolbar>
+    </AppBar>
   )
 };
 
-export default Navbar;
+function NavLink(props){
+  const classes = useStyles();
+  return (
+    <section>
+      <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+        <SportsMmaIcon />
+        <Typography variant="h6" className={classes.title}>
+          {props.navLink.title}
+        </Typography>
+      </IconButton>
+    </section>
+ )
+}
